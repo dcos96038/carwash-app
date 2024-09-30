e4e80c7c594f6e15ab0f8239d7718ec001c3f4ad
Diego Coscolla <diegocoscolla@gmail.com>
2024-09-30 19:43:54 -0300

    Add Map PoC (#1)
    * test map
    
    * Format code
    
    * Add biome and remove eslint
    
    * Add openstreetmap
    
    ---------
    
    Co-authored-by: andresvasile <andresvasile.av@gmail.com>

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,37 +1,37 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from "next";
+
 export default function manifest(): MetadataRoute.Manifest {
-  return {
-    name: 'Carwash App',
-    short_name: 'Carwash App',
-    description: 'Carwash App',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#000000',
-    icons: [
-      {
-        src: '/icon.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/icon.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-    ],
-    screenshots: [
-      {
-        src: '/screenshot.png',
-        sizes: '540x720',
-        type: 'image/png',
-      },
-      {
-        src: '/screenshot.png',
-        sizes: '540x720',
-        type: 'image/png',
-      },
-    ],
-  }
+	return {
+		name: "Carwash App",
+		short_name: "Carwash App",
+		description: "Carwash App",
+		start_url: "/",
+		display: "standalone",
+		background_color: "#ffffff",
+		theme_color: "#000000",
+		icons: [
+			{
+				src: "/icon.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				src: "/icon.png",
+				sizes: "512x512",
+				type: "image/png",
+			},
+		],
+		screenshots: [
+			{
+				src: "/screenshot.png",
+				sizes: "540x720",
+				type: "image/png",
+			},
+			{
+				src: "/screenshot.png",
+				sizes: "540x720",
+				type: "image/png",
+			},
+		],
+	};
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,57 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import "leaflet/dist/leaflet.css";
+
+import React, { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button>Click me</Button>
-    </div>
-  );
+	const [location, setLocation] = useState<{ lat: number; lng: number }>();
+
+	useEffect(() => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(showPosition, showError);
+		} else {
+			console.log("Geolocation is not supported by this browser.");
+		}
+	}, []);
+
+	const showPosition = (position: GeolocationPosition) => {
+		const coords = {
+			lat: position.coords.latitude,
+			lng: position.coords.longitude,
+		};
+		setLocation(coords);
+		console.log(`Latitude: ${coords.lat} Longitude: ${coords.lng}`);
+	};
+
+	const showError = (error: GeolocationPositionError) => {
+		console.log(`Error getting location: ${error.message}`);
+	};
+
+	if (!location) {
+		return <p>Loading location...</p>;
+	}
+
+	return (
+		<>
+			<MapContainer
+				center={[-26.9272191, -65.3408132]}
+				zoom={13}
+				scrollWheelZoom={false}
+				style={{ height: "100vh", width: "100%" }} // Estilos del mapa
+			>
+				<TileLayer
+					className="bg-blue-500"
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+				<Marker position={[-26.9272191, -65.3408132]}>
+					<Popup>
+						A pretty CSS3 popup. <br /> Easily customizable.
+					</Popup>
+				</Marker>
+			</MapContainer>
+		</>
+	);
 }
